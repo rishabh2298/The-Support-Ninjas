1961901f586dad7679744eb3c4748fdb467b7f84
rishabh2298 <rishabhmishra2298@gmail.com>
2023-02-23 17:32:47 +0530

    Mapping for all model done

--- The_Support_Ninjas/src/main/java/com/masai/app/model/Admin.java
+++ The_Support_Ninjas/src/main/java/com/masai/app/model/Admin.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -21,5 +22,6 @@ public class Admin {
 	private String firstName;
 	private String lastName;
 	private String email;
+
 
 }
--- The_Support_Ninjas/src/main/java/com/masai/app/model/Solution.java
+++ The_Support_Ninjas/src/main/java/com/masai/app/model/Solution.java
@@ -2,10 +2,15 @@ package com.masai.app.model;
 
 import java.time.LocalDate;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -21,5 +26,15 @@ public class Solution {
 	private Integer solutionId;
 	private String solutionDescription;
 	private LocalDate solutionDate;
+	
+	
+	@OneToOne(cascade = CascadeType.MERGE, fetch = FetchType.EAGER)
+	@JoinColumn(name = "issue_id")
+	private Issue issue;
+	
+	
+	@ManyToOne
+	@JoinColumn(name = "operator_id")
+	private Operator operator;
 	
 }
