af7ea6376a18b9d1c986d6ab891be5f2cb87acb8
rishabh2298 <rishabhmishra2298@gmail.com>
2023-02-23 18:55:24 +0530

    (for same structure) classes and interface are created with packages

---
+++ The_Support_Ninjas/src/main/java/com/masai/app/repository/OperatorRepository.java
@@ -0,0 +1,9 @@
+package com.masai.app.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.masai.app.model.Operator;
+
+public interface OperatorRepository extends JpaRepository<Operator, Integer> {
+
+}
