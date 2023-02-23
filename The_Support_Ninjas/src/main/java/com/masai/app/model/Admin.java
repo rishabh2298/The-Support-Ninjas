6529281c38669e17f2923833aef85166d622c0e6
rishabh2298 <rishabhmishra2298@gmail.com>
2023-02-23 15:57:15 +0530

    Mapping is done for all model classes

---
+++ The_Support_Ninjas/src/main/java/com/masai/app/model/Admin.java
@@ -0,0 +1,25 @@
+package com.masai.app.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Admin {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer adminId;
+	private String firstName;
+	private String lastName;
+	private String email;
+
+}
