fae36dbdb07a283bd38f5e345d85a49289b6ccbf
rishabh2298 <rishabhmishra2298@gmail.com>
2023-02-24 19:51:58 +0530

    Operator Service Implementation done

---
+++ The_Support_Ninjas/src/main/java/com/masai/app/exception/CallNotFoundException.java
@@ -0,0 +1,14 @@
+package com.masai.app.exception;
+
+public class CallNotFoundException extends RuntimeException {
+
+	public CallNotFoundException() {
+		
+	}
+	
+	
+	public CallNotFoundException(String message) {
+		super(message);
+	}
+	
+}
---
+++ The_Support_Ninjas/src/main/java/com/masai/app/service/CustomerService.java
@@ -0,0 +1,5 @@
+package com.masai.app.service;
+
+public interface CustomerService {
+
+}
